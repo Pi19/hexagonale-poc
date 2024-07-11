b246d2772825256d3ac8c9865f5d71126a1ba46c
Pi19 <hasimananjara@gmail.com>
2024-07-11 19:28:53 +0200

    feat : use other ErroMessages and remove hateoas problem

--- src/main/java/com/archi/hexagonale/application/exception/ControllerException.java
+++ src/main/java/com/archi/hexagonale/application/exception/ControllerException.java
@@ -2,32 +2,33 @@ package com.archi.hexagonale.application.exception;
 
 import com.archi.hexagonale.domain.exception.OperationNotAllowedException;
 import com.archi.hexagonale.domain.exception.RessourceNotFoundException;
-import org.springframework.hateoas.mediatype.problem.Problem;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Date;
+
 @ControllerAdvice
 public class ControllerException {
 
-    private Problem buildProblem(Exception ex, HttpStatus status) {
-        return Problem.create().withStatus(status).withDetail(ex.getMessage());
+    private ErrorMessages buildProblem(Exception ex, HttpStatus status) {
+        return new ErrorMessages(status.value() , new Date() , ex.getMessage());
     }
 
 
     @ExceptionHandler(value = RessourceNotFoundException.class)
-    public ResponseEntity<Problem> handleRessourceNotFoundException(RessourceNotFoundException exception){
+    public ResponseEntity<ErrorMessages> handleRessourceNotFoundException(RessourceNotFoundException exception){
         return new ResponseEntity<>(buildProblem(exception, HttpStatus.NOT_FOUND), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = OperationNotAllowedException.class)
-    public ResponseEntity<Problem> handleOperationNotAllowed(OperationNotAllowedException exception){
+    public ResponseEntity<ErrorMessages> handleOperationNotAllowed(OperationNotAllowedException exception){
         return new ResponseEntity<>(buildProblem(exception, HttpStatus.FORBIDDEN), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(value = BadRequestException.class)
-    public ResponseEntity<Problem> handleBadParameter(BadRequestException exception){
+    public ResponseEntity<ErrorMessages> handleBadParameter(BadRequestException exception){
         return new ResponseEntity<>(buildProblem(exception, HttpStatus.BAD_REQUEST), HttpStatus.BAD_REQUEST);
     }
 }
